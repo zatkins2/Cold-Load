117cb5a264e91b9ac7b50144c512231bb900652f
zatkins2 <46325652+zatkins2@users.noreply.github.com>
2019-08-08 10:59:25 -0400

    Create so_shield_cooldown_20190808.py

---
+++ Code/Thermal/so_shield_cooldown_20190808.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  8 10:33:02 2019
+
+@author: zatki
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+#define paths
+
+datapath = "../../../data/"
+figpath = "../../../../Figures/"
+
+#define logs to load
+vclfns = ["log_190727_124353"]
+diodefns = ["20190727_132000", "20190727_132151"]
